b0df5997bcce3d9feb72b28e26b72da880bea7a2
Srinithy676 <sp7324@srmist.edu.in>
2026-03-03 14:05:54 +0530

    Manya Added feature/UC1-WelcomePage

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,9 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to BookMyStay Application!");
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.println("3. Exit");
+    }
+}
